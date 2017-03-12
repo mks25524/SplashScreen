9b2573970c36c8f27c46e3fd57dbe066d59c096d
mks25524 <mks25524withme@gmail.com>
2017-03-12 14:35:16 +0600

    manifest setup for splash screen

---
+++ app/src/main/java/com/example/anew/splashscreen/splash.java
@@ -0,0 +1,16 @@
+package com.example.anew.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by new on 3/12/2017.
+ */
+
+public class splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+    }
+}
