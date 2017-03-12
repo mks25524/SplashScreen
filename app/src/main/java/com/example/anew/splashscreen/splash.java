3d4b257a12113b23a3adcbaff51b3832b3eb57cb
mks25524 <mks25524withme@gmail.com>
2017-03-13 01:19:56 +0600

    done but some problem

--- app/src/main/java/com/example/anew/splashscreen/splash.java
+++ app/src/main/java/com/example/anew/splashscreen/splash.java
@@ -1,7 +1,11 @@
 package com.example.anew.splashscreen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * Created by new on 3/12/2017.
@@ -12,5 +16,31 @@ public class splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashh);
+
+        final ImageView iv =(ImageView)findViewById(R.id.imageView);
+        final Animation an= AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+       // final Animation an2= AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+               // iv.startAnimation(an2);
+                finish();
+                Intent intent=new Intent(getBaseContext(),splash_screen.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/anew/splashscreen/splash_screen.java
+++ app/src/main/java/com/example/anew/splashscreen/splash_screen.java
@@ -1,7 +1,11 @@
 package com.example.anew.splashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splash_screen extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+
+
     }
 }
